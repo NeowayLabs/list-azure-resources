31975a16c0edb577bfbb50d391f44faab4678671
Luciano Antonio Borguetti Faustino <lucianoborguetti@gmail.com>
2018-08-08 11:44:52 -0300

    Add Azure Vm Size list

---
+++ bin/AzureVmSize.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+# This script using a Service Principal account to generate that list
+
+# Debug bash tags
+
+set -o errexit
+set -o nounset
+
+
+# Making the Azure login
+
+az login --service-principal --username $AZURE_SERVICE_PRINCIPAL \
+    --password $AZURE_CLIENT_SECRET \
+    --tenant $AZURE_TENANT_ID > /dev/null 2>&1
+
+
+# Using JQ to make a magic with the return JSON
+
+JQ_FILTER=".[] |
+    {
+        name: .name,
+        size: .hardwareProfile.vmSize
+    } | [ .name, .size ] | @csv"
+
+
+# Getting JSON with Subscriptions IDs
+
+function GetAzureSubscriptions(){
+
+    az account list | jq --raw-output ".[] | .id"
+}
+
+
+# Getting and cleaning a return JSON with Subscriptions IDs
+
+function GetAzureVMs(){
+
+    subscription=$1
+
+    az account set --subscription $subscription
+
+    subscription_name=$(az account show | jq --raw-output ".name")
+
+    az vm list | jq  --raw-output "$JQ_FILTER" | sed "s@^@\"$subscription_name\",@g"
+
+}
+
+# Looping with all the Subscriptions has the Service Principal can access
+
+for subscription in `GetAzureSubscriptions` ; do
+    GetAzureVMs $subscription
+done
